e275037b7e4936d8d8d61b6c8af02b2c79284f98
Dania "Gord1y" Hordiienko <95370285+Gord1y@users.noreply.github.com>
2023-09-29 21:05:12 +0300

    Create AppContext.tsx

---
+++ StarWarsApp/src/context/AppContext.tsx
@@ -0,0 +1,51 @@
+// AppContext.tsx
+import React, {
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useState
+} from 'react'
+
+import { Character } from '../interfaces/character.interface'
+
+interface AppContextType {
+  likedCharacters: Character[]
+  likeCharacter: (character: Character) => void
+  unlikeCharacter: (character: Character) => void
+}
+
+const AppContext = createContext<AppContextType | undefined>(undefined)
+
+export const useAppContext = () => {
+  const context = useContext(AppContext)
+  if (!context) {
+    throw new Error('useAppContext must be used within an AppContextProvider')
+  }
+  return context
+}
+
+export const AppContextProvider: React.FC<PropsWithChildren> = ({
+  children
+}) => {
+  const [likedCharacters, setLikedCharacters] = useState<Character[]>([])
+
+  const likeCharacter = (character: Character) => {
+    setLikedCharacters(prev => [...prev, character])
+  }
+
+  const unlikeCharacter = (character: Character) => {
+    setLikedCharacters(prev =>
+      prev.filter(likedCharacter => likedCharacter !== character)
+    )
+  }
+
+  const contextValue = {
+    likedCharacters,
+    likeCharacter,
+    unlikeCharacter
+  }
+
+  return (
+    <AppContext.Provider value={contextValue}>{children}</AppContext.Provider>
+  )
+}
